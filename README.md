07f3d7ba482d4853a555a2263b1c6d361a631a4d
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-10 19:53:26 +0300

    base settings

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 https://coursehunters.net/course/sozdayte-internet-magazin-s-react-i-graphql-za-90-minut
 
 https://github.com/reedbarger/ecommerce-react-graphql-stripe
+
+https://github.com/simotae14/EcommerceReactGraphqlStripe
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import App from "./App";
+import Navbar from "./components/Navbar";
+import App from "./components/App";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
 
 import * as serviceWorker from "./serviceWorker";
 
-import "./index.css";
+import "gestalt/dist/gestalt.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <Router>
+    <>
+      <Navbar />
+
+      <Switch>
+        <Route to="/" exact component={App} />
+        <Route to="/signin" component={Signin} />
+        <Route to="/signup" component={Signup} />
+        <Route to="/checkout" component={Checkout} />
+      </Switch>
+    </>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) {
+  module.hot.accept();
+}
